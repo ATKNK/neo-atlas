2fc5bb4c451390d091e1dcdd922d88cf0acde12d
Arthur Nakayama <arthurnakayama@gmail.com>
2025-04-08 11:04:56 -0300

    Initial Commit

---
+++ src/components/Map/Map.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { ReactComponent as Map1444 } from "../../assets/1444.svg";
+import './Map.css'
+
+function Map() {
+  return (
+    <div className="Map">
+      <Map1444 />
+    </div>
+  );
+}
+
+export default Map;
