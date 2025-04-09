6039c5a72fcb43585da192bbf4fc7f79c1db0b95
Arthur Nakayama <arthurnakayama@gmail.com>
2025-04-09 06:57:24 -0300

    Feat: Tooltip com Capital e Título, arquivo JSON com informações adicionado

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -1,29 +1,48 @@
 import React, { useState, useEffect } from "react";
 import { ReactComponent as Map1444 } from "../../assets/1444.svg";
-import './Map.css'
+import "./Map.css";
 
 function Map() {
-
   const [tooltip, setTooltip] = useState({
     visible: false,
     x: 0,
     y: 0,
-    content: ''
+    content: "",
   });
+
+  const [countryData, setCountryData] = useState({});
+
+  useEffect(() => {
+    fetch("/data/countries.json")
+      .then((res) => res.json())
+      .then((data) => setCountryData(data));
+  }, []);
 
   const handleMouseEnter = (e) => {
     const countryTag = e.target.id;
     const countryName = e.target.getAttribute("data-name");
+    const info = countryData[countryTag];
+
     setTooltip({
       visible: true,
       x: e.clientX,
       y: e.clientY,
       content: (
         <div className="tooltipContent">
-          <img src={`/flags/${countryTag}.png`} alt={countryName} className="flag"/>
+          <img
+            src={`/flags/${countryTag}.png`}
+            alt={countryName}
+            className="flag"
+          />
           <div className="tooltipTitle">{countryName}</div>
+          {info && (
+            <div className="tooltipExtra">
+              <div>Capital: {info.capital}</div>
+              <div>Title: {info.title}</div>
+            </div>
+          )}
         </div>
-      )
+      ),
     });
   };
 
@@ -31,37 +50,37 @@ function Map() {
     setTooltip((prev) => ({
       ...prev,
       x: e.clientX,
-      y: e.clientY
+      y: e.clientY,
     }));
   };
 
   const handleMouseLeave = () => {
-    setTooltip((prev) => ({...prev, visible: false}));
+    setTooltip((prev) => ({ ...prev, visible: false }));
   };
 
   useEffect(() => {
-    const countries = document.querySelectorAll('.country');
-    countries.forEach(country => {
-      country.addEventListener('mouseenter', handleMouseEnter);
-      country.addEventListener('mousemove', handleMouseMove);
-      country.addEventListener('mouseleave', handleMouseLeave);
+    const countries = document.querySelectorAll(".country");
+    countries.forEach((country) => {
+      country.addEventListener("mouseenter", handleMouseEnter);
+      country.addEventListener("mousemove", handleMouseMove);
+      country.addEventListener("mouseleave", handleMouseLeave);
     });
 
-    return() => {
-      countries.forEach(country => {
-        country.removeEventListener('mouseenter', handleMouseEnter);
-        country.removeEventListener('mousemove', handleMouseMove);
-        country.removeEventListener('mouseleave', handleMouseLeave);
+    return () => {
+      countries.forEach((country) => {
+        country.removeEventListener("mouseenter", handleMouseEnter);
+        country.removeEventListener("mousemove", handleMouseMove);
+        country.removeEventListener("mouseleave", handleMouseLeave);
       });
     };
-  }, []);
+  }, [countryData]);
 
   return (
     <div className="Map">
       <Map1444 />
-      <div 
-        className={`tooltip ${tooltip.visible ? 'visible' : ''}`}
-        style={{top: tooltip.y + 10, left: tooltip.x + 10}}
+      <div
+        className={`tooltip ${tooltip.visible ? "visible" : ""}`}
+        style={{ top: tooltip.y + 10, left: tooltip.x + 10 }}
       >
         {tooltip.content}
       </div>
